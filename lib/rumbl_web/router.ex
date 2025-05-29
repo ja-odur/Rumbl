3cbd85c526aabf87da9f95bd731f54f3c328ec94
ja-odur <odurjoseph8@gmail.com>
2025-05-29 13:12:41 +0300

    Add index page

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -17,7 +17,7 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
