8f2d7caf0d0e9359634b54b2b6fe61a2307d4dbe
ja-odur <odurjoseph8@gmail.com>
2025-06-20 10:15:35 +0300

    Add user_socket endpoint

--- lib/rumbl_web/endpoint.ex
+++ lib/rumbl_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule RumblWeb.Endpoint do
+  alias Phoenix.Channel
   use Phoenix.Endpoint, otp_app: :rumbl
 
   # The session will be stored in the cookie and signed,
@@ -14,6 +15,11 @@ defmodule RumblWeb.Endpoint do
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
+
+  # Video Channel
+  socket "/socket", RumblWeb.UserSocket,
+    websocket: true,
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
