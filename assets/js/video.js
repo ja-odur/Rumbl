9a96b77407182ad1a11823cb7f87a7fdd02c69c7
ja-odur <odurjoseph8@gmail.com>
2025-06-22 00:42:20 +0300

    Add video annotations

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,30 +1,73 @@
 import Player from "./player";
 
-
 let Video = {
   init(socket, element) {
-    if (!element) { return }
-    
+    if (!element) {
+      return;
+    }
+
     let playerId = element.getAttribute("data-player-id");
     let videoId = element.getAttribute("data-id");
-    
+
     socket.connect();
-    
+
     Player.init(element.id, playerId, () => {
       this.onReady(videoId, socket);
-    })
+    });
   },
-  
+
   onReady(videoId, socket) {
     let msgContainer = document.getElementById("msg-container");
-    let msgInput = document.getElementById("msg-input")
-    let postButton = document.getElementById("msg-submit")
-    let videoChannel = socket.channel("videos:" + videoId)
+    let msgInput = document.getElementById("msg-input");
+    let postButton = document.getElementById("msg-submit");
+    let videoChannel = socket.channel("videos:" + videoId);
+
+    postButton.addEventListener("click", (e) => {
+      let payload = { body: msgInput.value, at: Player.getCurrentTime() };
+
+      videoChannel
+        .push("new_annotation", payload)
+        .receive("error", (e) => console.log(e));
+      msgInput.value = "";
+    });
+
+    videoChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp);
+    });
+
+    videoChannel
+      .join()
+      .receive("ok", (resp) => console.log("joined the video channel", resp))
+      .receive("error", (reason) => console.log("join failed", reason));
+  },
+
+  renderAnnotation(msgContainer, { user, body, at }) {
+    let template = document.createElement("div");
     
-    videoChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp))
-      .receive("error", reason => console.log("join failed", reason))
-  }
-}
+    template.innerHTML = `
+      <a href="#" data-seek="${this.escapeInput(at)}">
+        [${this.formatTime(at)}]
+        <b>${this.escapeInput(user.username)}</b>: ${this.escapeInput(body)}
+      </a>
+      `;
+
+    msgContainer.appendChild(template);
+    msgContainer.scrollTop = msgContainer.scrollHeight;
+
+  },
+  
+  formatTime(at) {
+      let date = new Date(null);
+      date.setSeconds(at / 1000);
+      return date.toISOString().substr(14, 5);
+    },
+  
+    escapeInput(userInputString) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(userInputString));
+      return div.innerHTML;
+    }
+
+};
 
 export default Video;
--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -1,7 +1,22 @@
 defmodule RumblWeb.VideoChannel do
-	use RumblWeb, :channel
-	
-	def join("videos:" <> video_id, _params, socket) do
-	  {:ok, assign(socket, :video_id, String.to_integer(video_id))}
-	end
+  use RumblWeb, :channel
+
+  def join("videos:" <> video_id, _params, socket) do
+    :timer.send_interval(5_000, :ping)
+    {:ok, socket}
+  end
+
+  def handle_in("new_annotation", params, socket) do
+    broadcast!(
+      socket,
+      "new_annotation",
+      %{
+        user: %{username: "annon"},
+        body: params["body"],
+        at: params["at"]
+      }
+    )
+    
+    {:reply, :ok, socket}
+  end
 end
