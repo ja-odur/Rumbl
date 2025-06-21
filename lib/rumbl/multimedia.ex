c0e5133ff8b3cbe1602605af0eaa3ca7b28e9585
ja-odur <odurjoseph8@gmail.com>
2025-06-22 02:31:47 +0300

    Persist annotations in the db

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -5,11 +5,27 @@ defmodule Rumbl.Multimedia do
 
   import Ecto.Query, warn: false
   alias Rumbl.Repo
-  
+
   alias Rumbl.Accounts
   alias Rumbl.Multimedia.Category
   alias Rumbl.Multimedia.Video
-  
+  alias Rumbl.Multimedia.Annotation
+
+  def annotate_video(%Accounts.User{id: user_id}, video_id, attrs) do
+    %Annotation{video_id: video_id, user_id: user_id}
+    |> Annotation.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_annotations(%Video{} = video) do
+    Repo.all(
+      from a in Ecto.assoc(video, :annotations),
+        order_by: [asc: a.at, asc: a.id],
+        limit: 500,
+        preload: [:user]
+    )
+  end
+
   @doc """
   Create a category.
 
@@ -18,7 +34,7 @@ defmodule Rumbl.Multimedia do
   def create_category!(name) do
     Repo.insert!(%Category{name: name}, on_conflict: :nothing)
   end
-  
+
   def list_alphabetical_categories do
     Category
     |> Category.alphabetical()
@@ -119,23 +135,22 @@ defmodule Rumbl.Multimedia do
   def change_video(%Video{} = video, attrs \\ %{}) do
     Video.changeset(video, attrs)
   end
-  
+
   ## User scoped queries
-  
+
   def list_user_videos(%Accounts.User{} = user) do
     Video
     |> user_videos_query(user)
     |> Repo.all()
   end
-  
+
   def get_user_video!(%Accounts.User{} = user, id) do
     Video
     |> user_videos_query(user)
     |> Repo.get!(id)
   end
-  
+
   defp user_videos_query(query, %Accounts.User{id: user_id}) do
     from(v in query, where: v.user_id == ^user_id)
   end
-  
 end
--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -1,22 +1,34 @@
 defmodule RumblWeb.VideoChannel do
+  
   use RumblWeb, :channel
+  alias Rumbl.{Accounts, Multimedia}
 
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
-    {:ok, socket}
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  end
+  
+  def handle_in(event, params, socket) do
+    user = Accounts.get_user!(socket.assigns.user_id)
+    handle_in(event, params, user, socket)
   end
 
-  def handle_in("new_annotation", params, socket) do
-    broadcast!(
-      socket,
-      "new_annotation",
-      %{
-        user: %{username: "annon"},
-        body: params["body"],
-        at: params["at"]
-      }
-    )
-    
-    {:reply, :ok, socket}
+  def handle_in("new_annotation", params, user, socket) do
+    case Multimedia.annotate_video(user, socket.assigns.video_id, params) do
+      {:ok, annotation} ->
+        broadcast!(
+          socket, 
+          "new_annotation",
+          %{
+            id: annotation.id,
+            user: RumblWeb.UserJSON.show(user),
+            body: annotation.body,
+            at: annotation.at
+          }
+        )
+        {:reply, :ok, socket}
+        
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
