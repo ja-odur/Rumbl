a41ad8c9f084458d8ec9231827c218ea779d3394
ja-odur <odurjoseph8@gmail.com>
2025-06-22 18:40:35 +0300

    Track online users using Phx Presence

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,3 +1,5 @@
+import { Presence } from "phoenix";
+
 import Player from "./player";
 
 let Video = {
@@ -20,9 +22,21 @@ let Video = {
     let msgContainer = document.getElementById("msg-container");
     let msgInput = document.getElementById("msg-input");
     let postButton = document.getElementById("msg-submit");
+    let userList = document.getElementById("user-list");
     let lastSeenId = 0;
     let videoChannel = socket.channel("videos:" + videoId, () => {
       return { last_seen_id: lastSeenId };
+    });
+
+    let presence = new Presence(videoChannel);
+
+    presence.onSync(() => {
+      userList.innerHTML = presence
+        .list((id, { user: user, metas: [first, ...rest] }) => {
+          let count = rest.length + 1;
+          return `<li>${user.username}: (${count})</li>`;
+        })
+        .join("");
     });
 
     postButton.addEventListener("click", (e) => {
